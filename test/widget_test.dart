8d5977d1bc203f3507c4cc777bd1ab05ee3e8dce
Alphonso darel <alphonsodarely@gmail.com>
2026-05-28 09:54:33 +0100

    feat:(routing):mise en place des routes (Continuer comme visiteur ou se connecter)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,10 +3,10 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:scolar_mobile/main.dart';
 
 void main() {
-  testWidgets('L\'app démarre et affiche le showcase', (tester) async {
+  testWidgets('L\'app démarre et affiche l\'onboarding', (tester) async {
     await tester.pumpWidget(const ScolarApp());
     await tester.pumpAndSettle();
 
-    expect(find.text('Design System'), findsOneWidget);
+    expect(find.text('Suivez vos notes'), findsOneWidget);
   });
 }
